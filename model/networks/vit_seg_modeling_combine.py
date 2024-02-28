Fix scaling and rotation in VisionTransformer class

model/networks/vit_seg_modeling_combine.py
@@ -463,7 +463,7 @@ class VisionTransformer(nn.Module):
             nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.Flatten(),
-            nn.Linear(25088, 6),
+            nn.Linear(25088, 5),
             nn.Sigmoid()
         )
 
@@ -484,8 +484,8 @@ class VisionTransformer(nn.Module):
         
         x0, y0 = input[:, 0].reshape(-1, pts), input[:, 1].reshape(-1, pts)
         mu = torch.einsum('ijk->jki', torch.stack(((x0)*h, (y0)*w)))
-        scale = input[:, 2:4].reshape(-1, pts, 2) * h/2 + 1
-        rot_angle = input[:, 4].reshape(-1, pts) * math.pi/4
+        scale = torch.tile(input[:, 2:3].reshape(-1, pts, 1) * h/2 + 1, (1, 1, 2))
+        rot_angle = input[:, 3].reshape(-1, pts) * math.pi/4
 
         rotation = torch.zeros((b, pts, 2, 2)).to(input.device)
         rotation[:, :, 0, 0] = torch.cos(rot_angle[:])
@@ -545,8 +545,8 @@ class VisionTransformer(nn.Module):
             print('feature')
             exit(0)
         gauss = self.gaussian_2d(gaussian_features, 64, 64)
-        gauss_1 = (gauss * gaussian_features[:,5:6]).reshape(b, 64, 64)
-        gauss_2 = (gauss * (1- gaussian_features[:,5:6])).reshape(b, 64, 64)
+        gauss_1 = (gauss * gaussian_features[:,4:5]).reshape(b, 64, 64)
+        gauss_2 = (gauss * (1- gaussian_features[:,4:5])).reshape(b, 64, 64)
         mask_logits= torch.stack([gauss_1, gauss_2], dim=1)
         mask_logits = torch.nn.functional.interpolate(mask_logits, size=(224, 224), mode='bicubic', align_corners=True)
 