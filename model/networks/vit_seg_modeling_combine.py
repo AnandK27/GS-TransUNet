Fix Gaussian calculation in VisionTransformer class

model/networks/vit_seg_modeling_combine.py
@@ -546,8 +546,8 @@ class VisionTransformer(nn.Module):
             exit(0)
         gauss = self.gaussian_2d(gaussian_features, 64, 64)
         print(gauss.shape, gaussian_features[:,5].shape)
-        gauss_1 = (gauss * gaussian_features[:,5]).reshape(b, 64, 64)
-        gauss_2 = (gauss * (1- gaussian_features[:,5])).reshape(b, 64, 64)
+        gauss_1 = (gauss * gaussian_features[:,5:6]).reshape(b, 64, 64)
+        gauss_2 = (gauss * (1- gaussian_features[:,5:6])).reshape(b, 64, 64)
         mask_logits= torch.stack([gauss_1, gauss_2], dim=1)
         mask_logits = torch.nn.functional.interpolate(mask_logits, size=(224, 224), mode='bicubic', align_corners=True)
 