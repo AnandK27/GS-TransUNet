Fix Gaussian mask calculation and print shape of variables

model/networks/vit_seg_modeling_combine.py
@@ -545,7 +545,7 @@ class VisionTransformer(nn.Module):
             print('feature')
             exit(0)
         gauss = self.gaussian_2d(gaussian_features[:, :], 64, 64)
-
+        print(gauss.shape, gaussian_features.shape)
         mask_logits= torch.stack([(gauss * gaussian_features[:,5]).reshape(b, 64, 64), (gauss * gaussian_features[:,6]).reshape(b, 64, 64)], dim=1)
         mask_logits = torch.nn.functional.interpolate(mask_logits, size=(224, 224), mode='bicubic', align_corners=True)
 