148aaea01a004a2bf06d790cbd2eec8ee0526093
Anand Kumar <63339285+AnandK27@users.noreply.github.com>
2024-03-02 15:33:01 -0800

    Update test frequency in trainer function

--- engine.py
+++ engine.py
@@ -258,7 +258,7 @@ def trainer(args, model):
                 writer.add_scalar('val/vjac_score', np.nanmean(vjac_score), epoch)
 
         with torch.no_grad():
-            if epoch+1 % 50 == 0:
+            if epoch+1 % 10 == 0:
                 print('start test!')
 
                 [vacc, vdice, vsen, vspe, vjac_score, total_acc, m_acc, s_acc, dic] = val_mode_seg_multi_scale(args,
