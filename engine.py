a1fe4fcc1a05219e2bf9e5e197cd7163fc2e332a
Anand Kumar <63339285+AnandK27@users.noreply.github.com>
2024-03-03 09:30:20 -0800

    Update loss function to use FocalLoss for classification

--- engine.py
+++ engine.py
@@ -17,13 +17,14 @@ from utils.metrics import compute_sdf, dice_loss
 from utils.learning_rate import adjust_learning_rate, get_current_consistency_weight
 from utils.runtime import random_resize, get_cls_label, name_list_to_cls_label
 from utils.validate import val_mode_seg, val_mode_seg_multi_scale
-from utils.loss import MyLabelSmoothingLoss
+from utils.loss import MyLabelSmoothingLoss, FocalLoss
 
 
 # get loss function
 mse_loss = nn.MSELoss()
 l1_loss = nn.L1Loss()
 ce_loss = nn.CrossEntropyLoss()
+focal_loss = FocalLoss(num_classes=3)
 weight_ce_loss_for_class_m = nn.CrossEntropyLoss(weight=torch.Tensor([1.0, 1.0, 1.0])).cuda()  # 第1类的权重调高
 label_smooth_ce_loss = MyLabelSmoothingLoss(classes=2, smoothing=0.0)
 
@@ -135,7 +136,7 @@ def trainer(args, model):
             dis_to_mask = torch.sigmoid(-1500 * dt_preds)
 
             ### cls loss
-            cls_loss = ce_loss(cls_logits, cls_label)
+            cls_loss = focal_loss(cls_logits, cls_label)
 
             ### consistency loss
             consistency_loss = torch.mean(
--- utils/loss.py
+++ utils/loss.py
@@ -1,5 +1,36 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+from utils import one_hot_embedding
+from torch.autograd import Variable
+
+class FocalLoss(nn.Module):
+    def __init__(self, num_classes=20):
+        super(FocalLoss, self).__init__()
+        self.num_classes = num_classes
+
+    def focal_loss(self, x, y):
+        '''Focal loss.
+
+        Args:
+          x: (tensor) sized [N,D].
+          y: (tensor) sized [N,].
+
+        Return:
+          (tensor) focal loss.
+        '''
+        alpha = 0.25
+        gamma = 2
+
+        t = one_hot_embedding(y.data.cpu(), self.num_classes)  # [N,21]
+        t = Variable(t).cuda()  # [N,20]
+
+        p = x.sigmoid()
+        pt = p*t + (1-p)*(1-t)         # pt = p if t > 0 else 1-p
+        w = alpha*t + (1-alpha)*(1-t)  # w = alpha if t > 0 else 1-alpha
+        w = w * (1-pt).pow(gamma)
+        return F.binary_cross_entropy_with_logits(x, t, w, size_average=False)
+
 
 
 class MyLabelSmoothingLoss(nn.Module):
