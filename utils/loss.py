995bcdd97085b8a76db18b603e6da32268a7a032
Anand Kumar <63339285+AnandK27@users.noreply.github.com>
2024-03-03 09:57:48 -0800

    Refactor focal_loss function in utils/loss.py

--- utils/loss.py
+++ utils/loss.py
@@ -16,32 +16,29 @@ def one_hot_embedding(labels, num_classes):
     y = torch.eye(num_classes)  # [D,D]
     return y[labels]            # [N,D]
 
-class FocalLoss(nn.Module):
-    def __init__(self, num_classes=20):
-        super(FocalLoss, self).__init__()
-        self.num_classes = num_classes
 
-    def forward(self, x, y):
-        '''Focal loss.
 
-        Args:
-          x: (tensor) sized [N,D].
-          y: (tensor) sized [N,].
+def focal_loss(self, x, y, num_classes=3):
+    '''Focal loss.
 
-        Return:
-          (tensor) focal loss.
-        '''
-        alpha = 0.25
-        gamma = 2
+    Args:
+        x: (tensor) sized [N,D].
+        y: (tensor) sized [N,].
 
-        t = one_hot_embedding(y.data.cpu(), self.num_classes)  # [N,21]
-        t = Variable(t).cuda()  # [N,20]
+    Return:
+        (tensor) focal loss.
+    '''
+    alpha = 0.25
+    gamma = 2
 
-        p = x.sigmoid()
-        pt = p*t + (1-p)*(1-t)         # pt = p if t > 0 else 1-p
-        w = alpha*t + (1-alpha)*(1-t)  # w = alpha if t > 0 else 1-alpha
-        w = w * (1-pt).pow(gamma)
-        return F.binary_cross_entropy_with_logits(x, t, w, size_average=False)
+    t = one_hot_embedding(y.data.cpu(), num_classes)  # [N,21]
+    t = Variable(t).cuda()  # [N,20]
+
+    p = x.sigmoid()
+    pt = p*t + (1-p)*(1-t)         # pt = p if t > 0 else 1-p
+    w = alpha*t + (1-alpha)*(1-t)  # w = alpha if t > 0 else 1-alpha
+    w = w * (1-pt).pow(gamma)
+    return F.binary_cross_entropy_with_logits(x, t, w, size_average=False)
 
 
 
