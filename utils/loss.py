982c060619caafa8214495c1b4cb31b936aa345e
Anand Kumar <63339285+AnandK27@users.noreply.github.com>
2024-03-03 10:20:45 -0800

    Update loss function to use detached weights

--- utils/loss.py
+++ utils/loss.py
@@ -38,7 +38,7 @@ def focal_loss(x, y, num_classes=3):
     pt = p*t + (1-p)*(1-t)         # pt = p if t > 0 else 1-p
     w = alpha*t + (1-alpha)*(1-t)  # w = alpha if t > 0 else 1-alpha
     w = w * (1-pt).pow(gamma)
-    return F.binary_cross_entropy_with_logits(x, t, w, size_average=False)
+    return F.binary_cross_entropy_with_logits(x, t, w.detach(), size_average=False)
 
 
 
